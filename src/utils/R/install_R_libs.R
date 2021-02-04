2d472e6e2add9db33f92407bd7b9cd71d096932d
Enrico Gaffo <enrico.gaffo@gmail.com>
2021-02-04 14:49:23 +0100

    removed unused libraries from installation

--- src/utils/R/install_R_libs.R
+++ src/utils/R/install_R_libs.R
@@ -1,26 +1,22 @@
 #!/usr/bin/env Rscript
 
-#prepacks <- c("BH", "svglite")
-#
-#install.packages(prepacks, repos="http://cran.r-project.org", dependencies = T)
+packs <- c("optparse", "data.table", #"BH", "svglite",  
+	   "plyr",
+           #"scales", "reshape2", "DT",
+           #"ggplot2", "ggthemes", "viridis",
+           #"RSvgDevice", "rmarkdown", "knitr", "VennDiagram",
+	   #"tidyr", "pheatmap", 
+           #"stringi",
+           "bedr")
+ncpus <- as.integer(Sys.getenv('CPUS'))
+if(is.na(ncpus)){
+    ncpus <- 1
+}
+install.packages(packs, repos="https://cloud.r-project.org/", dependencies = T, Ncpus = ncpus)
 
-packs <- c("BH", "svglite", "optparse", "plyr",
-           "scales", "reshape2", "DT",
-           "ggplot2", "ggthemes", "viridis",
-           "RSvgDevice", "rmarkdown", "knitr", "VennDiagram",
-	   "tidyr", "bedr", "stringi", "pheatmap")
-
-install.packages(packs, repos="http://cran.r-project.org", dependencies = T)
-
-#for(pack in packs){
-#  install.packages(pack, repos="http://cran.r-project.org", dependencies = T)
-#}
-
-#source("http://bioconductor.org/biocLite.R")
-#biocLite(pkgs = c("DESeq2", "ReportingTools", "ballgown"))
-## Bioconductor >= v3.9 (with R >= 3.6)
 if (!requireNamespace("BiocManager", quietly = TRUE))
     install.packages("BiocManager")
 #BiocManager::install(c("DESeq2", "ReportingTools", "ballgown"))
-BiocManager::install(c("ReportingTools", "ballgown"))
+#BiocManager::install(c("ReportingTools", "ballgown"))
+BiocManager::install(c("ballgown"), Ncpus = ncpus)
 
